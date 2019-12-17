2db8796645996af11710ce33a4e77df61d07b303
Chris Behrens <cbehrens@gmail.com>
2019-12-17 13:38:51 -0600

    Made more Fort Worth

--- SimpleFunction/Function1.cs
+++ SimpleFunction/Function1.cs
@@ -26,7 +26,7 @@ namespace SimpleFunction
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Howdy, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
